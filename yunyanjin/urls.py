b8bb6058ddf4074e9e0dc1c5bb93068e83a54a37
mRcfps <1402491442@qq.com>
2017-08-24 09:32:03 +0800

    Bug fix in site urls.

--- yunyanjin/urls.py
+++ yunyanjin/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^users/', include('users.urls', namespace='users')),
     url(r'^admin/', admin.site.urls),
-] += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
