f1a2fb8aecd589dbaf8d36e1a1d0f1c1a45fbb1f
mRcfps <1402491442@qq.com>
2017-08-28 15:05:06 +0800

    Add OrderAdmin.

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from orders.models import Order
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+
+    list_display = ('order_id', 'user', 'status', 'destination')
+    search_fields = ('order_id',)
+
+
+admin.site.register(Order, OrderAdmin)
