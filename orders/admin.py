77aae679bdcbf8818facf3b79f6fbf38c6763320
mRcfps <1402491442@qq.com>
2017-08-29 16:39:38 +0800

    Minor improvements of OrderAdmin.

--- orders/admin.py
+++ orders/admin.py
@@ -5,7 +5,8 @@ from orders.models import Order
 class OrderAdmin(admin.ModelAdmin):
 
     list_display = ('order_id', 'user', 'status', 'destination')
-    search_fields = ('order_id',)
+    search_fields = ('order_id', 'user', 'destination')
+    list_filter = ('status',)
 
 
 admin.site.register(Order, OrderAdmin)
