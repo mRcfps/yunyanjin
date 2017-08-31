de0a51098872a7f19fc8331773d3c796e17d1739
mRcfps <1402491442@qq.com>
2017-08-31 16:49:25 +0800

    Add product photo to CartItemSerializer.

--- cart/serializers.py
+++ cart/serializers.py
@@ -7,11 +7,21 @@ from shop.serializers import ItemSerializer
 class CartItemSerializer(serializers.ModelSerializer):
 
     item = ItemSerializer()
+    photo = serializers.SerializerMethodField()
 
     class Meta:
         model = CartItem
-        fields = ('item', 'quantity')
-        read_only_field = ('item',)
+        fields = ('item', 'quantity', 'photo')
+        read_only_field = ('item', 'photo')
+
+    def get_photo(self, cart_item):
+        request = self.context.get('request')
+        try:
+            photo = cart_item.item.product.image.url
+            return request.build_absolute_uri(photo)
+        except ValueError:
+            # this product has no avatar
+            return None
 
 
 class CartItemEditSerializer(serializers.ModelSerializer):
